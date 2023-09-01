bf64befc8aa8f93a34240e96f0fba75e1440a26b
nicocarrasco703 <ncarrasco@dc.uba.ar>
2023-09-01 12:37:31 -0300

    Falta que el padre reciba el numero y lo imprima

--- Taller 2/pipes/Ejercicio2/anillo_alu.c
+++ Taller 2/pipes/Ejercicio2/anillo_alu.c
@@ -4,12 +4,11 @@
 #include <sys/wait.h>
 #include <time.h>
 #include <unistd.h>
+#include <signal.h>
 
 #include "constants.h"
 
 int generate_random_number() { return (rand() % 50); }
-
-void sigchld_handler() { read printf }
 
 int main(int argc, char **argv) {
   // Funcion para cargar nueva semilla para el numero aleatorio
@@ -25,12 +24,9 @@ int main(int argc, char **argv) {
     exit(0);
   }
 
-  /* COMPLETAR */
   printf(
       "Se crearán %i procesos, se enviará el caracter %i desde proceso %i \n",
       n, buffer, start);
-
-  /* COMPLETAR */
 
   pid_t *children = malloc(sizeof(*children) * n);
 
@@ -41,45 +37,73 @@ int main(int argc, char **argv) {
     pipe(pipes[i]);
   }
 
-  close(pipes[0][0]);
-  write(pipes[0][1], &n, sizeof(n));  // escribo el mensaje por el pipe
+  write(pipes[n][PIPE_WRITE], &n, sizeof(int));  // escribo el mensaje por el pipe
 
-  // signal();
-
-  // primer hijo
-  children[0] = fork();
-  if (children[0] < 0)
-    exit(EXIT_FAILURE);
-  else if (children[0] == 0) {
-  }
-
-  // hijos intermedios
   for (int i = 0; i < n; i++) {
     children[i] = fork();
-    if (children[i] < 0)
-      exit(EXIT_FAILURE);
+    if (children[i] < 0) exit(EXIT_FAILURE);
     else if (children[i] == 0) {
-      close(pipes[i][0]);  // cierro los extremos de lectura al sucesor
+      int pre = i % n;
+      int suc = (i+1) % n;
 
+      close(pipes[suc][PIPE_READ]);  // cierro el extremo de lectura al sucesor
+      close(pipes[pre][PIPE_WRITE]); // cierro el extremo de escritura del predecesor
       for (int k = 0; k < n; k++) {  // cierro los demas pipes
-        if (k != i) {
-          close(pipes[k][0]);
-          close(pipes[k][1]);
+        if (k != pre && k != suc) {
+          close(pipes[k][PIPE_READ]);
+          close(pipes[k][PIPE_WRITE]);
         }
       }
-
-      if (i == start) {  // si soy el proceso que inicia
-        int numero = 0;
-        read(pipes[0][0], &numero,
-             sizeof(numero));  // del pipe padre leo el numero
-        int num_secreto = generate_random_number();  // genero numero secreto
-        write(pipes[i][1], &num_secreto,
-              sizeof(int));  // escribo el numero secreto al pipe sucesor
+      if(i != start){ // si no soy el proceso inicial cierro los pipes padre
+        close(pipes[n][PIPE_READ]);
+        close(pipes[n][PIPE_WRITE]);
       }
-
-      for (int j = 0; j < count; j++) {
-        if
+      int numero;
+      int it = 1;
+      
+      while(1){
+        if (i == start){ // si soy el proceso que inicia, leo el pipe n (del padre) y arranco
+          int num_secreto = generate_random_number();  // genero numero secreto
+          write(pipes[n][PIPE_WRITE], &num_secreto, sizeof(int)); // escribo el pipe del padre con el numero secreto
+          if (it == 1){ // si es la primera iteracion
+            int num_inicial;
+            it++;
+            read(pipes[n][PIPE_READ], &num_inicial, sizeof(int)); // leo el pipe del padre para obtener el numero inicial
+            write(pipes[suc][PIPE_WRITE], &num_inicial, sizeof(int)); // escribo el pipe del sucesor con el nuevo valor
+          } else {
+            read(pipes[pre][PIPE_READ], &numero, sizeof(int)); // leo el pipe del predecesor
+            if (numero >= num_secreto){ // si el numero es mayor o igual al secreto
+              printf("Proceso %d: Numero secreto alcanzado (%d >= %d)\n", i, numero, num_secreto);
+              break;
+            } else {
+              numero++;
+              write(pipes[suc][PIPE_WRITE], &numero, sizeof(int)); // escribo el pipe del sucesor con el nuevo valor
+            }
+          }
+        } else { // si no soy el proceso inicial
+          read(pipes[pre][PIPE_READ], &numero, sizeof(int)); // leo el pipe del predecesor
+          numero++;
+          write(pipes[suc][PIPE_WRITE], &numero, sizeof(int)); // escribo el pipe del sucesor con el nuevo valor
+        }
       }
+      exit(EXIT_SUCCESS);
     }
   }
+
+  for (int i = 0; i < n; i++){ // cierro los pipes del padre
+    close(pipes[i][PIPE_READ]);
+    close(pipes[i][PIPE_WRITE]);  
+  }
+  // mato a todos los hijos una vez que terminen
+  for (int i = 0; i < n; i++) {
+    waitpid(children[i], &status, 0);
+  }
+
+  int num_secreto;
+  read(pipes[n][PIPE_READ], &num_secreto, sizeof(int)); // leo el pipe del padre para obtener el numero secreto del proceso inicial
+  printf("Numero secreto: %d\n", num_secreto);
+  
+  free(children);
+
+  exit(EXIT_SUCCESS);
 }
